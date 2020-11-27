e6cab47388ec15944e8539a9d4eeb7ae95eb6019
Austin Graham <austingraham731@gmail.com>
2020-11-27 13:57:45 -0600

    add about message

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -7,7 +7,9 @@ use structopt::StructOpt;
 pub mod dataserver;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "rd")]
+#[structopt(
+    name = "rd",
+    about = "Welcome to Rubber Duck! This CLI assists you in completing all your necessary tasks related to the Rubber Duck environment.")]
 pub struct RD {
     #[structopt(subcommand)]
     cmd: Command
